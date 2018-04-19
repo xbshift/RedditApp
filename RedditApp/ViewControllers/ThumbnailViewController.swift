e8bf48c21b3a736cab6a5770a9891b57a71b2bea
dc <>
2018-04-19 06:10:10 -0700

    Implement image save

--- RedditApp/ViewControllers/ThumbnailViewController.swift
+++ RedditApp/ViewControllers/ThumbnailViewController.swift
@@ -5,7 +5,9 @@ import UIKit
 class ThumbnailViewController: UIViewController {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func saveImageAction(_ sender: Any) {
+        if let image = thumbnailImageView.image {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
     }
 }
